59cdf1c16fe97ddd9f07d0957a4b3f5329097d33
Abhishek Gogoi <abhisat495@gmail.com>
2025-01-08 18:50:40 +0530

    Added port in server file

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-const express = require("express")
-const cors = require("cors")
-const dotenv = require("dotenv")
-const transactionRoutes = require('./routes/transactionRoutes')
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const transactionRoutes = require("./routes/transactionRoutes");
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const port = process.env.PORT
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.use('/api/transactions', transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
